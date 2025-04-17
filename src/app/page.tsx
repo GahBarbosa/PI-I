aa14ea6c67452e4864561908cb051a1d0a94a64b
Gabriel Barbosa <gahbarbosa7@gmail.com>
2025-04-17 16:34:54 -0300

    eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,11 +21,11 @@ export default async function Home() {
 
   return (
     <div className="min-h-screen bg-black text-amber-50 grid grid-rows-3 grid-cols-1 p-4 col-s">
-      <List title='Lista de Clientes' dados={clientes} />
-      <List title='Lista de Produtos' dados={produtos} />
-      <List title='Lista de Insumos' dados={insumos} />
-      <List title='Lista de Fornecedores' dados={fornecedores} />
-      <List title='Lista de Pedidos' dados={pedidos} />
+      <List code='cli' title='Lista de Clientes' dados={clientes} />
+      <List code='pro' title='Lista de Produtos' dados={produtos} />
+      <List code='ins' title='Lista de Insumos' dados={insumos} />
+      <List code='for' title='Lista de Fornecedores' dados={fornecedores} />
+      <List code='ped' title='Lista de Pedidos' dados={pedidos} />
     </div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,12 +5,13 @@ type Item = {
 };
 
 type Props = {
+  code: string;
   title: string;
   dados: Item[];
 };
 
 
-export default function List({ title, dados }: Props) {
+export default function List({ code, title, dados }: Props) {
   return (
     <div className='flex flex-col'>
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] ">
@@ -18,9 +19,9 @@ export default function List({ title, dados }: Props) {
       </h1>
       <div className='grid grid-cols-2'>
         {dados.map((objeto, index) => (
-          <ul key={index} style={{ marginBottom: '1rem' }}>
+          <ul key={code + '_' + index} style={{ marginBottom: '1rem' }}>
             {Object.entries(objeto).map(([chave, valor]) => (
-              <li key={chave}>
+              <li key={code + '_' + chave}>
                 <strong>{chave}:</strong> {String(valor)}
               </li>
             ))}
